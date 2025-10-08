5a828b4f1a2a6d266c897da84cb832276ae1bac6
Nasir-AhmadZ <118723649+Nasir-AhmadZ@users.noreply.github.com>
2025-10-08 12:06:06 +0100

    Update schemas.py

--- Login/schemas.py
+++ Login/schemas.py
@@ -1,9 +1,12 @@
 # Login/schemas.py
 from pydantic import BaseModel, EmailStr, constr, conint
 
-class User(BaseModel):
-    user_id:int
+class UserLogin(BaseModel):
     username:constr(min_length=2,max_length=50)
-    password:constr(min_length=8,max_length=8)
+    password:constr(max_length=8)
     email:EmailStr
     
+class UserRegister(BaseModel):
+    username:constr(min_length=2,max_length=50)
+    password:constr(max_length=8)
+    email:EmailStr
